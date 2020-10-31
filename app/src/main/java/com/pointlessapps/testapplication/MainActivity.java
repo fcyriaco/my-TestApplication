3f253ab3f20c93dfdc0d646d40d97786165330aa
fcyriaco@yahoo.com.br <fcyriaco@yahoo.com.br>
2020-10-31 00:28:58 -0300

    New button and text.

--- app/src/main/java/com/pointlessapps/testapplication/MainActivity.java
+++ app/src/main/java/com/pointlessapps/testapplication/MainActivity.java
@@ -3,12 +3,26 @@ package com.pointlessapps.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // relaciona com activity_main.xml
         setContentView(R.layout.activity_main);
+    }
+
+    // funcao para tratar o click no botao
+    public void onButtonClick(View view) {
+        // cria o objeto txtBox relacionado com o campo txtMessage da activity_main.txt
+        TextView txtBox = findViewById(R.id.txtMessage);
+        // cria o objeto edtTxtName para entrada de texto.
+        EditText edtTxtName = findViewById(R.id.editTxtName);
+        // modifica o texto na caixa.
+        txtBox.setText("Hello " + edtTxtName.getText().toString());
     }
 }
