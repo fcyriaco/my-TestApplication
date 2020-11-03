ece5e68cc03cef352cf41bf7fcc838e06971f848
fcyriaco@yahoo.com.br <fcyriaco@yahoo.com.br>
2020-11-02 22:01:11 -0300

    New features.

--- app/src/main/java/com/pointlessapps/testapplication/MainActivity.java
+++ app/src/main/java/com/pointlessapps/testapplication/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,7 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText edtTxtFirstName;
     private EditText edtTxtLastName;
     private EditText edtTxtEmail;
-
+    private CheckBox checkBox;
+    private RadioGroup rgButtons;
+    private boolean letterCase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +32,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(this);
+
+        checkBox = findViewById(R.id.chkBox);
         txtFirstName = findViewById(R.id.txtFirstName);
         txtLastName = findViewById(R.id.txtLastName);
         txtEmail = findViewById(R.id.txtEmail);
         edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
         edtTxtLastName = findViewById(R.id.edtTxtLastName);
         edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        rgButtons = findViewById(R.id.rgButtons);
+        rgButtons.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbUpperCase:
+                        letterCase = true;
+                        break;
+                    case R.id.rbLowerCase:
+                        letterCase = false;
+                        break;
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.btnRegister:
-                txtFirstName.setText("First Name: " + edtTxtFirstName.getText().toString());
-                txtLastName.setText("Last Name: " + edtTxtLastName.getText().toString());
-                txtEmail.setText("Email: " + edtTxtEmail.getText().toString());
-                Toast.makeText(this, "Registered!", Toast.LENGTH_SHORT).show();
+                if(checkBox.isChecked()) {
+                    if(letterCase) {
+                        txtFirstName.setText("First Name: " + edtTxtFirstName.getText().toString().toUpperCase());
+                        txtLastName.setText("Last Name: " + edtTxtLastName.getText().toString().toUpperCase());
+                        txtEmail.setText("Email: " + edtTxtEmail.getText().toString().toUpperCase());
+                    } else {
+                        txtFirstName.setText("First Name: " + edtTxtFirstName.getText().toString().toLowerCase());
+                        txtLastName.setText("Last Name: " + edtTxtLastName.getText().toString().toLowerCase());
+                        txtEmail.setText("Email: " + edtTxtEmail.getText().toString().toLowerCase());
+                    }
+                    Toast.makeText(this, "Registered!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Can't registry this!", Toast.LENGTH_SHORT).show();
+                }
                 break;
             default:
                 break;
